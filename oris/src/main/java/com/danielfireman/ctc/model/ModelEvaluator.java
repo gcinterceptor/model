8184c9bb4ff35ee372c21b09b485c22b2406101b
Daniel Fireman <danielfireman@gmail.com>
2020-01-13 20:10:14 -0300

    Got the number of tokens

--- oris/src/main/java/com/danielfireman/ctc/model/ModelEvaluator.java
+++ oris/src/main/java/com/danielfireman/ctc/model/ModelEvaluator.java
@@ -1,26 +1,33 @@
 package com.danielfireman.ctc.model;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
+import org.oristool.analyzer.log.AnalysisLogger;
 import org.oristool.models.gspn.GSPNSteadyState;
 import org.oristool.models.gspn.GSPNTransient;
+import org.oristool.models.pn.PetriStateFeature;
 import org.oristool.models.stpn.RewardRate;
 import org.oristool.models.stpn.TransientSolution;
-import org.oristool.models.stpn.trans.RegTransient;
-import org.oristool.models.stpn.trees.DeterministicEnablingState;
 import org.oristool.petrinet.Marking;
 import org.oristool.petrinet.PetriNet;
+import org.oristool.simulator.Sequencer;
+import org.oristool.simulator.Sequencer.SequencerEvent;
+import org.oristool.simulator.SequencerObserver;
+import org.oristool.simulator.rewards.ContinuousRewardTime;
+import org.oristool.simulator.rewards.RewardTime;
+import org.oristool.simulator.stpn.STPNSimulatorComponentsFactory;
 import org.oristool.util.Pair;
 
 public class ModelEvaluator {
 	static final double step = 0.1;
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		PetriNet pn = new PetriNet();
 		Marking m = new Marking();
 		Model.build(pn, m);
@@ -28,52 +35,97 @@ public class ModelEvaluator {
 		// https://github.com/oris-tool/sirio/wiki/Generalized-Stochastic-Petri-Nets
 
 		/*
-		 * ######## STEAD-STATE ANALYSIS #######
+		 * ######## SIMULATION #######
 		 */
-		System.out.println("Steady State Analysis");
-		Map<Marking, Double> dist = GSPNSteadyState.builder().build().compute(pn, m);
-		System.out.println(dist.entrySet().stream().map(e -> String.format("%s -> %.6f%n", e.getKey(), e.getValue()))
-				.collect(Collectors.joining()));
+		System.out.println("Simulation");
+		Sequencer s = new Sequencer(pn, m, new STPNSimulatorComponentsFactory(), new AnalysisLogger() {
+			@Override
+			public void log(String message) {
+				System.out.println(message);
+			}
+
+			@Override
+			public void debug(String message) {
+				// System.out.println(message);
+			}
+		});
+
+		class TokenCounter implements SequencerObserver {
+			public List<Marking> markings = new LinkedList<>();
+			public List<BigDecimal> sojournTime = new LinkedList<>();
+			public List<BigDecimal> timestamps = new LinkedList<>();
+			private Sequencer sequencer;
+			private RewardTime time;
+			private BigDecimal duration;
+			private BigDecimal elapsedTime = BigDecimal.ZERO;
+
+			public TokenCounter(Sequencer s, double step, double duration) {
+				this.sequencer = s;
+				this.time = new ContinuousRewardTime(new BigDecimal(step));
+				this.duration = new BigDecimal(duration);
+				this.timestamps.add(BigDecimal.ZERO);
+				this.markings.add(s.getInitialMarking());
+				this.sequencer.addObserver(this);
+			}
+
+			@Override
+			public void update(SequencerEvent event) {
+				switch (event) {
+				case RUN_START:
+					this.sequencer.addCurrentRunObserver(this);
+					break;
+				case FIRING_EXECUTED:
+					Marking m = this.sequencer.getLastSuccession().getChild().getFeature(PetriStateFeature.class)
+							.getMarking();
+					this.markings.add(m);
+
+					BigDecimal sojourn = this.time.getSojournTime(this.sequencer.getLastSuccession());
+					this.elapsedTime = this.elapsedTime.add(sojourn);
+					this.timestamps.add(elapsedTime);
+					this.sojournTime.add(sojourn);
+					if (this.sequencer.getCurrentRunElapsedTime().compareTo(duration) >= 0) {
+						this.sequencer.removeCurrentRunObserver(this);
+						this.sequencer.removeObserver(this);
+					}
+					break;
+	            case RUN_END:
+	            	this.sequencer.removeCurrentRunObserver(this);
+				}
+			}
+		}
+
+		TokenCounter tc = new TokenCounter(s, step, 10);
+		s.simulate();
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < tc.markings.size(); i++) {
+			Marking marking = tc.markings.get(i);
+			builder.append(tc.timestamps.get(i));
+			builder.append(" ");
+			for (String name : marking.getNonEmptyPlacesNames()) {
+				builder.append(name);
+				builder.append(":");
+				builder.append(marking.getTokens(name));
+				builder.append(" | ");
+			}
+			builder.append("\n");
+		}
+		System.out.println(builder);
 
 		/*
 		 * ######## TRANSIENT ANALYSIS #######
+		 * 
 		 */
 		Pair<Map<Marking, Integer>, double[][]> tr = GSPNTransient.builder().timePoints(0.0, 5.0, step).build()
 				.compute(pn, m);
 		double[][] trValues = tr.second();
-		System.out.println(Arrays.deepToString(trValues));
 		Map<Marking, Integer> trMarkings = tr.first();
-		
 
-		/*
-		 * ######## REWARDS #######
-		 */
-
-		// Follow:
-		// https://github.com/oris-tool/sirio/blob/5015a2fba7f84f959127a909a1eebad43e424a07/sirio/src/test/java/org/oristool/models/gspn/client/AbsorbingRewardTest.java#L90
-		System.out.println("\n\n## Rewards ##\n\n");
-		System.out.println("Mean Response Time: ");
-		
-		TransientSolution<Marking, Marking> solution = TransientSolution.fromArray(trValues, step, trMarkings, m);
-
-		boolean cumulative = true;
-		TransientSolution<Marking, RewardRate> reward = TransientSolution.computeRewards(
-				cumulative, solution, "If(B>0,1.0,0);B;t");
-		
-		
-		//for (int t = 0; t < reward.getSolution().length; t++) {
-		//	System.out.println(reward.getSolution()[t][0][0]);
-		//}
-		//System.out.println(Arrays.deepToString(reward.getSolution()));
-		System.out.println(reward.toString());
-		System.out.println(reward.computeIntegralSolution().toString());
-		
-		
 		/*
 		 * ######## FULL PROB #######
 		 */
 		System.out.println("\n\n## Transient Analysis ##\n\n");
 
+		System.out.println(trValues.length);
 		// Pretty-printing results.
 		Map<Integer, Marking> byID = new HashMap<>();
 		for (Entry<Marking, Integer> e : trMarkings.entrySet()) {
@@ -89,6 +141,31 @@ public class ModelEvaluator {
 			}
 			System.out.println();
 		}
+
+		/*
+		 * ######## STEAD-STATE ANALYSIS #######
+		 */
+		System.out.println("Steady State Analysis");
+		Map<Marking, Double> dist = GSPNSteadyState.builder().build().compute(pn, m);
+		System.out.println(dist.entrySet().stream().map(e -> String.format("%s -> %.6f%n", e.getKey(), e.getValue()))
+				.collect(Collectors.joining()));
+
+		/*
+		 * ######## REWARDS #######
+		 */
+
+		// Follow:
+		// https://github.com/oris-tool/sirio/blob/5015a2fba7f84f959127a909a1eebad43e424a07/sirio/src/test/java/org/oristool/models/gspn/client/AbsorbingRewardTest.java#L90
+		System.out.println("\n\n## Rewards ##\n\n");
+		System.out.println("Mean Response Time: ");
+
+		TransientSolution<Marking, Marking> solution = TransientSolution.fromArray(trValues, step, trMarkings, m);
+		solution.getColumnStates();
+
+		boolean cumulative = true;
+		TransientSolution<Marking, RewardRate> reward = TransientSolution.computeRewards(cumulative, solution,
+				"If(B>0,1.0,0);B;t");
+		System.out.println(reward.toString());
 
 	}
 }
